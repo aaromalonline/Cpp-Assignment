f3ab957dafeb6cf7472d7e167e0a33fe5c42ffa6
MadonaRoseIrenius <madonaroseirenius2005@gmail.com>
2025-02-25 10:42:06 +0530

    Update program4.cpp

--- program4.cpp
+++ program4.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Name: Kilian\nClass: BTech ECE\nRegister No: XXXXXXXX\n";
+    cout << "Name: Madona Rose Irenius\nClass: ECE A\nRegister No: 24102232\n";
     int num, largest = 0, smallest = 9;
     cout << "Enter a number: ";
     cin >> num;

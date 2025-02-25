1a9eb74fc156a0548a7d619026c1116013d7c29b
MadonaRoseIrenius <madonaroseirenius2005@gmail.com>
2025-02-25 10:39:36 +0530

    Update program10-1.cpp

--- program10-1.cpp
+++ program10-1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Name: Kilian\nClass: BTech ECE\nRegister No: XXXXXXXX\n";
+    cout << "Name: Madona Rose Irenius\nClass: ECE A\nRegister No: 24102232\n";
     string str = "C++easy";
     for (int i = 1; i <= str.length(); i++) {
         cout << str.substr(0, i) << endl;
